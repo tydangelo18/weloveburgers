9099737fd9a24fc3c88e8c995e7cc7028a06ced9
Ty D'Angelo <tydangelo18@gmail.com>
2020-09-08 12:19:49 -0500

    Built burger model and routes for CRUD operations.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,68 @@
+// Import Express
+let express = require("express");
+
+// Import Router
+let router = express.Router();
+
+// Import the burger Model
+let burger = require('../models/burger.js');
+
+
+// Routes for CRUD operations
+
+// Get all burgers from DB
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        let hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+// Create a burger
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "burger_name", "munched"
+    ], [
+        req.body.burger_name, req.body.munched
+    ], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+// Update a burger
+router.put("/api/burgers/:id", function(req, res) {
+    let condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        munched: req.body.munched
+    }, condition, function(result) {
+        // If no rows were changed, Id does not exist (404 Error)
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+// Delete a burger
+router.delete("/api/burgers/:id", function(req, res) {
+    let condition = "id = " + req.params.id;
+
+    burger.delete(condition, function(result) {
+        // If no rows were changed, Id does not exist (404 Error)
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+// Export routes for server.js to use
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,30 @@
+// Import ORM
+let orm = require("../config/orm.js");
+
+let burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
+
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+
+    delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
