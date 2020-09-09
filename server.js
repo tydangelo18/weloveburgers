77bd1cb4a95bdbc9bce893583d4bce1c727f3da6
Ty D'Angelo <tydangelo18@gmail.com>
2020-09-09 11:45:17 -0500

    Setup Views: setup handlebars and created buttons to submit burgers.

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 // Require Express
 const express = require("express");
 
+// Set up express app
+let app = express();
+
+// Set Handlebars
+let exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
