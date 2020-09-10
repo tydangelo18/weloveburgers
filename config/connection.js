b48eafda4ff3e40bb5f272154470a260a710f615
Ty D'Angelo <tydangelo18@gmail.com>
2020-09-10 18:04:48 -0500

    Heroku Deployment.

--- config/connection.js
+++ config/connection.js
@@ -7,21 +7,15 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "password",
-        database: "burger_db"
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'burger_db' || process.env.DATABASE
     });
 };
 
 // Make MySQL connection
-connection.connect(function(err) {
-    if (err) {
-        console.error("Error Connecting: " + err.stack);
-        return;
-    }
-    console.log("Connected as id " + connection.threadId);
-});
+connection.connect();
 
 // Export connection for ORM to use
 module.exports = connection;
