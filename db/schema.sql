adface2d5044c01555696c1e97e9a419742592dc
Ty D'Angelo <tydangelo18@gmail.com>
2020-09-10 18:17:39 -0500

    MySQL syntax edit.

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE burger_db;
 USE burger_db;
 
 CREATE TABLE burgers (
-    id int NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     burger_name varchar(255) NOT NULL,
     munched BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
