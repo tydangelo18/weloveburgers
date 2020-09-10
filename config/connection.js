b995470ad8893ad5a7c31567f49f99af757f662f
Ty D'Angelo <tydangelo18@gmail.com>
2020-09-10 17:34:16 -0500

    JawsDB Configuration.

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 // Import mysql dependency
 const mysql = require('mysql');
 
-// Set up MySQL connection
-let connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burger_db"
-});
+// Set up MySQL connection and Hook with JawsDB
+let connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "burger_db"
+    });
+};
 
 // Make MySQL connection
 connection.connect(function(err) {
